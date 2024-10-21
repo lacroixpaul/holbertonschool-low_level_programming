e17c24293320e74a2043351facf39c6affc10f4b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 14:19:28 +0000

    Script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * int main : point d'entree du programme 
+ * function should display a string of characteres including speciale one 
  */
 int main(void)
 {
-	/** 
-	 * puts affiche une chaine de caracteres dont un caractere special
-	 */
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	/**
-	 * return value 0 si le programme s'est bien executer
-	 */
 	return (0);
 }
