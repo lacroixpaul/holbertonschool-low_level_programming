11d22b34aabb9f1ade6a83b63be17200e040e83d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 14:21:36 +0000

    Script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * function should display a string of characteres including speciale one 
+ * function should display a string of characteres including speciale one
  */
 int main(void)
 {
