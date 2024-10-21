72983ddcd6586f418a398e775d2ad21ea66dcbd1
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 14:29:42 +0000

    Script 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Point d'entrée principal du programme.
+ *
+ * Affiche une chaine de caracteres.
+ *
+ * Return: Retourne 0 si l'exécution s'est terminée avec succès.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
