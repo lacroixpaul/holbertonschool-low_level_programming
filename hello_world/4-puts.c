19fe0843251165635d7d7d9b1c918e2929258576
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 13:57:51 +0000

    Script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib>
 
 int main(void) {
-    puts("Programming is like building a multilingual puzzle,\n");
+    puts("\"Programming is like building a multilingual puzzle");
     return 0;
-}
-
