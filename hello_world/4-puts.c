969ce68b12ae8d7a3f2a36596e978d1100e8d3ce
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 14:15:45 +0000

    Script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * La fonction doit afficher une chaine de caractère et un caractère spécial
+ * int main : point d'entree du programme 
  */
 int main(void)
 {
+	/** 
+	 * puts affiche une chaine de caracteres dont un caractere special
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
+	
+	/**
+	 * return value 0 si le programme s'est bien executer
+	 */
 	return (0);
 }
