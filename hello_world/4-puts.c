48de2cfdeab1c95c542a2b0b20a26f39677ea785
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-21 14:10:15 +0000

    Script 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * La fonction doit afficher une chaine de caractère et un caractère spécial
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
