89a44f22592655db4925892ced218893818fbd85
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-22 09:49:26 +0000

    Script 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Display alphabet.
+ * Return: Always 0 (Success).
+ **/
+
+int main(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	putchar(lettre);
+	return (0);
+}
