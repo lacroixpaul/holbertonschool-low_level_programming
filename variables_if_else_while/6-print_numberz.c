cb03a8e328de56c6e7919c11635047754cb48d02
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-22 11:45:30 +0000

    Script 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Display all single digit numbers of base 10 starting from 0.
+ * Return: Always 0 (Success).
+ **/
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
