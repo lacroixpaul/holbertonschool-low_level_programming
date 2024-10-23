3397f13bc3ba8509a6c34d37eefa535f531ed813
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 13:57:07 +0000

    Script 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ */
+
+void jack_bauer(void)
+{
+int heures, minutes, a, b, c, d;
+
+	for (heures = 0; heures < 24; heures++)
+{
+	for (minutes = 0; minutes < 60; minutes++)
+{
+	int a = (heures / 10);
+	int b = (heures % 10);
+	int c = (minutes / 10);
+	int d = (minutes % 10);
+
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(':');
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+}
+}
+}
