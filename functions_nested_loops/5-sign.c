d247fd3ecdf58d4e0539e2ba915deceb816bc063
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 10:07:47 +0000

    Script 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,8 +14,6 @@ int print_sign(int n)
 	if (n > 0)
 	{
 	_putchar('+');
-	_putchar(',');
-	_putchar(' ');
 	return (1);
 	_putchar('\n');
 	}
@@ -23,16 +21,12 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 	_putchar('0');
-	_putchar(',');
-	_putchar(' ');
 	return (0);
 	_putchar('\n');
 	}
 	else 
 	{
 	_putchar('-');
-	_putchar(',');
-	_putchar(' ');
 	return (-1);
 	_putchar('\n');
 }
