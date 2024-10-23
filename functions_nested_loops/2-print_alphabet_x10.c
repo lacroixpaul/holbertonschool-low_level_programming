3145ae0c05b0c56da49c37343c303186aa74314d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 08:34:25 +0000

    Script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - display alphabet 10 times in lowercase.
+ * Return: void.
+ **/
+
+void print_alphabet_x10(void)
+{
+	char lettre = 'a';
+	int i = '0';
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+	{
+		_putchar(lettre);
+	}
+	_putchar('\n');
+	}
