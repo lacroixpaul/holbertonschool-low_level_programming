5cd129d15ba6af191af72073a4a7989806c5e588
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 09:04:33 +0000

    Script 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower  checks for lowercase character.
+ * Returns 1 if c is lowercase, returns 0 otherwise.
+ **/
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+}
+}
