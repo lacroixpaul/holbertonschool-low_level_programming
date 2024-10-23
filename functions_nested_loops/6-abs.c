33e428d11aadd3627cb6a7fbe4272a59a2e23aa4
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 11:24:18 +0000

    Script 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Calcule la valeur absolue d'un entier
+ * @int n: L'entier dont on veut calculer la valeur absolue
+ *
+ * Return: La valeur absolue de n
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n < '0')
+	{
+		(return -n);
+	}
+	else
+	{
+		(return n);
+	}
+}
