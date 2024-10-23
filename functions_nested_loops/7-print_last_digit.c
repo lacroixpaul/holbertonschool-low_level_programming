f795d804d6407c9a2776ab4b928c734a736a891b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 11:56:12 +0000

    Script 7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit -  prints the last digit of a number.
+ * @number: L'entier dont on veut trouver le dernier digit. *
+ * @lastdigit: dernier digit en fonction de number.
+ * Return: return last digit.
+ */
+
+int print_last_digit(int number, lastdigit)
+
+{
+lastdigit = number % 10;
+return (lastdigit);
+}
