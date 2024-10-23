e9026ae0dbd0bb466f37adcc257549cd2916c047
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-23 09:58:56 +0000

    Script 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - affiche le signe du nombre
+ *
+ * @n: Le caractère à vérifier
+ *
+ * Return: 1 -1 ou 0 si le nombre est positif négatif ou égal à 0.
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+	return (1);
+	}
+
+	else if (n == 0)
+	{
+	_putchar('0');
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+
+	else if (n < 0)
+	{
+	_putchar('-');
+	_putchar(',');
+	_putchar(' ');
+	_putchar('\n');
+	return (-1);
+}
+}
