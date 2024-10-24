908acb427082eca864b2f97a1111248c9649facf
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-24 12:14:49 +0000

    Script 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: premier entier.
+ * @b: second entier.
+ * Return: results
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
