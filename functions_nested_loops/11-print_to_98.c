1d396f515a114b0d23cd8154745796c5cd6cbe75
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-24 13:52:41 +0000

    Script 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,30 +8,33 @@
 
 void print_to_98(int n)
 {
-		if (n < 98)
-		{
+	if (n < 98)
+	{
 		for (n = n; n < 99; n++)
 		{
-		if (n != 98)
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			printf("%d\n", n);
+			}
+		}
+	else if (n == 98)
+	{
+			printf("%d\n", n);
+	}
+	else
+	{
+		for (n = n; n > 98; n--)
 		{
-		printf("%d, ", n);
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else 
+			{
+				printf("%d\n", n);
+			}
 		}
-		printf("%d\n", n);
-		}
-		}
-		else if (n == 98)
-		{
-		printf("%d\n", n);
-		}
-		else
-		{
-		for (n = n; n > 97; n--)
-		{
-		if (n != 98)
-		{
-		printf("%d, ", n);
-		}
-		printf("%d\n", n);
-		}
-		}
+	}
 }
