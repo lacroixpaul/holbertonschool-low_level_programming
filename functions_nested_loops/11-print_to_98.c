a65e3f598187fc01f7a987c4bd627672f97ee64a
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-24 12:45:55 +0000

    Script 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		for (n = n; n < 99; n++)
 		{
-		printf("%d", n);
+		printf("%d, ", n);
 		}}
 	else if (n == 98)
 		{
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	else
 		{
 		for (n = n; n > 97; n--)
-		 printf("%d", n);
+		 printf("%d, ", n);
 		}}
 		putchar('\n');
 }
