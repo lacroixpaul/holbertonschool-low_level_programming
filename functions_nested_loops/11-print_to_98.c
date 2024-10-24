9952380feaa23f38e852f09d75bb454eb749b08e
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-24 12:29:47 +0000

    Script 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: nombre d'entree.
+ */
+
+void print_to_98(int n)
+{
+	if (n < '98')
+	{
+		for (n = n; n == 98; n++)
+		{
+		_putchar(n);
+		}
+		else if (n == 0)
+		{
+		_putchar(n);
+		}
+		else
+		{
+		for (n = n; n == 98; n--)
+		_putchar(n)
+		}}
+		_putchar('\n');
+}
