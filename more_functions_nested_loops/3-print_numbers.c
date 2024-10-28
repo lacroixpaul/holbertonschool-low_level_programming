49bf21fd3c153b68c97bb9177928820369d58946
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 09:33:24 +0000

    Script 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9.
+ **/
+
+void print_numbers(void)
+{
+	for (int i = 0, int < 10, i++)
+		_putchar(i + '0');
+	_putchar(\n);
+}
