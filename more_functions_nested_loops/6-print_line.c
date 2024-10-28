cb0be307c03983443f868fcd3cf5536a84c3d08c
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 10:46:03 +0000

    Script 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line-  draws a straight line in the terminal.
+ * @n : number of _.
+ **/
+
+void print_line(int n)
+{
+	int n;
+	int i;
+
+	for (i = 1; i <= n; i++)
+		if (n > 0)
+			_putchar('_');
+	_putchar('\n');
+}
