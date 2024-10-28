429eabb472c168ed62734b598cc796c740ff6c64
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 10:08:51 +0000

    Script 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  print_more_numbers -  prints 10 times the numbers, from 0 to 14.
+ **/
+
+void more_numbers(void);
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+		if (j > 9)
+		{
+		putchar((j / 10) + '0');
+		}
+		putchar((j % 10) + '0');
+	}
+		putchar('\n');
+	}
+}
