8050f3c6522d4342fcc618668a28d8630913db8d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 12:40:59 +0000

    Script 9

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square-  prints a square.
+ * @size : number of lines and column.
+ **/
+
+void print_square(int size)
+{
+	int i = 0;
+	int j = 0;
+
+	if size < 1
+	{
+		_putchar('\n');
+	}
+	else if size > 0
+	{
+		for (i = 0; i < size; i++)
+	{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		_putchar('\n');
+}
+}
+}
