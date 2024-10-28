f5b52ad82af2f80db8b389139a794c4787610a0d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 13:30:07 +0000

    Script 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,14 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0 && i != 100)
 			printf("FizzBuzz ");
 		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
 		if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz ");
+		if (i == 100)
+			printf("Buzz");
 		else if (i % 3 != 0 && i % 5 != 0)
 		printf("%d ", i);
 }
