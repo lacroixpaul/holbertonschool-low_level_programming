af1aa2982fc7097528887e2d32515b760ed9ebe4
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-28 12:10:15 +0000

    Script 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal-  draws a straight line in the terminal.
+ * @n : number of lines.
+ **/
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j = 0;
+
+		if (n < 0)
+		{
+			putchar('\n');
+		}
+		else if (n > 0)
+		{
+			for (i = 0; i <= n; i++)
+		{
+				for (j = 0; j <= i; j++)
+				{
+					putchar(' ');
+				}
+		putchar(92);
+		putchar('\n');
+}
+}
+}
