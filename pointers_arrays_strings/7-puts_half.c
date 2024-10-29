b53930729aea6819897e525dbbf6a1e95025ab2e
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-29 15:15:19 +0000

    Script 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -13,15 +13,14 @@ int i, n;
 	for (i = 0;  str[i] != '\0'; i++)
 	{
 	}
-	i--;
 	n = i / 2;
-	if (n % 2 == 1)
+		if (n % 2 == 1)
 	{
-		 n = (n - 1);
+		n = (n - 1);
 	}
-		 for (; str[n] != '\0'; n++)
+		for (; str[n] != '\0'; n++)
 	{
-		_putchar(str[n]);
+		_putchar(str[(n + 1)]);
 	}
 	_putchar('\n');
 }
