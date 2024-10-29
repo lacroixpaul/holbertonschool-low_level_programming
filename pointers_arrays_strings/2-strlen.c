f346efdc57575eb3b557f44c9fe8eb36ae056ed7
lacroixpaul <paul.lacroix892@outlook.fr>
2024-10-29 10:03:24 +0000

    Script 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to analyze.
+ * Return: lenght of s.
+ **/
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0;  s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
