ac43f2d69708235f8e8b34530e3bd19925070c25
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-01 18:55:04 +0000

    Script 8

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string into rot13.
+ *@s: string to convert.
+ *Return: the converted string.
+ **/
+
+char *rot13(char *s)
+{
+int i, j;
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (s[i] == letters[j])
+			s[i] = rot[j];
+		}
+	}
+	return (s);
+}
