fd7f738181c210913b8d468abb8edd3f7f6cdff8
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-01 18:36:35 +0000

    Script 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,23 +10,30 @@
 char *cap_string(char *str)
 {
 int i;
+int j;
+char separateur[] = " \t\n,;.!?\"(){}";
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i == 0)
 		{
-			if ((str[i] >= 'a' && str[i] <= 'z'))
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
 				str[i] = str[i] - 32;
-				continue;
+			}
+			continue;
 		}
-		 if (str[i] == ' ' || '\t' || '\n' || ',' || ';' || '.' || '!' || '?' || '"' || '(' || ')' || '{' || '}' )
+	for (j = 0; separateur[j] != '\0'; j++)
+	{
+		if (str[i] == separateur[j])
 		{
-			 i++
-				 if (str[i] >= 'a' && str[i] <= 'z')
-				{
-					str[i] = str[i] - 32;
-					continue;
-				}
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+			break;
 		}
+	}
 	}
 	return (str);
 }
