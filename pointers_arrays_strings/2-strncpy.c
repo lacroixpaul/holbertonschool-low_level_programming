4abc93d5ce882419a3046e84fc4248bce97dd40b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-01 10:13:49 +0000

    Script 2

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copies a string.
+ * @dest: string of destinatation.
+ * @src : string to copy.
+ * @n: bite limit.
+ * Return: dest.
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
