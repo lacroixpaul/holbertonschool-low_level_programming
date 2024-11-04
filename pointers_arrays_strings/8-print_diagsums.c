475da38eeff22a0e8d909bc702ed6176282fe54b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-04 15:23:53 +0000

    Script 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j;
+	int i;
 	long int sum_a = 0, sum_b = 0;
 
 	if (size == 1)
@@ -19,19 +19,10 @@ void print_diagsums(int *a, int size)
 	else if (size > 1)
 	{
 		for (i = 0; i < size; i++)
-	{
-			for (j = 0; j < size; j++)
 		{
-				if (i == j)
-				{
-					sum_a = sum_a + a[i * size + i];
-				}
-				else if (i + j == size - 1)
-				{
-					sum_b = sum_b + a[i * size + (size - i - 1)];
-				}
+		sum_a += a[i * (size + 1)];
+		sum_b += a[i * size + (size - 1 - i)];
 		}
-	}
 	}
 	printf("%ld, ", sum_a);
 	printf("%ld\n", sum_b);
