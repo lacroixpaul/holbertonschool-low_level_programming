bca96fca195ff2116285f7e9b0e69387d59d6077
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-05 10:45:19 +0100

    Script 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion -  prints a string in reverse.
+ * @s : string to prints.
+ **/
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
