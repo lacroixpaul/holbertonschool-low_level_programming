ac413becdc6c93545c57f7ee4495e3a93f626f8b
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-06 10:03:17 +0100

    Script 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - number of arguments passed into it.
+ * @argc : number of argument.
+ * @argv : array containing argument.
+ * Return: 0.
+ **/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+return (0);
+}
