a5b5db58591625073899f5930848ad38e72460b6
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-06 09:49:53 +0100

    Script 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of the programme.
+ * @argc : number of argument.
+ * @argv : array containing argument.
+ * Return: 0.
+ **/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+return (0);
+}
