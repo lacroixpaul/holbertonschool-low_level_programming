64521b8ffff1639e5d3f1d8a764029b7cbfdff0a
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-12 10:53:58 +0100

    Script 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat- concatenates two strings.
+ * @s1 : content of origin.
+ * @s2 : content to add.
+ * Return: a pointer to the array, or NULL if it fails.
+ **/
+
+char *str_concat(char *s1, char *s2)
+{
+	int size_1 = 0;
+	int size_2 = 0;
+	char *newstr;
+	int i = 0;
+	int j = 0;
+
+	if (s1 == 0 || s2 == 0)
+	{
+		return (NULL);
+	}
+	for (; s1[size_1] != '\0'; size_1++)
+	{
+	}
+	for (; s2[size_2] != '\0'; size_2++)
+	newstr = (char *)malloc((size_1 + size_2 + 1) * sizeof(char));
+
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+		for (; i < size_1; i++)
+		{
+			newstr[i] = s1[i];
+		}
+	for (; j < size_2; j++)
+	{
+		newstr[i + j] = s2[j];
+	}
+	newstr[i + j] = '\0';
+	return (newstr);
+}
