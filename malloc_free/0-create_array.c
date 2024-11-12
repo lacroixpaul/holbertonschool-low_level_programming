89715927a623244adad02237e1b3a5a83accba5d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-12 09:49:02 +0100

    Script 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,13 +14,13 @@ char *create_array(unsigned int size, char c)
 	char *array = (char *)malloc(size * sizeof(char));
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
 	if (size == 0 || array == 0)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[i] = c;
 	}
 return (array);
 }
