6b28061d2ebcdd1a0f00b8afcf04a25ef24c74d5
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-13 13:34:17 +0100

    Script 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *new_dog - creates a new dog..
+ * @name : of the dog.
+ * @age : of the dog.
+ * @owner : name of the owner.
+ * Return: new dog.
+ **/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	{
+	new_dog->name = strdup(name);
+	new_dog->age = age;
+	new_dog->owner = strdup(owner);
+	}
+	return (new_dog);
+}
