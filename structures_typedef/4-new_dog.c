61eec128e0e0e9eb302aeacc88549a0ff9a4d69e
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-13 14:59:28 +0100

    Script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: string to analyze.
+ * Return: lenght of s.
+ **/
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0;  s[length] != '\0'; length++)
+	{
+	}
+	return (length);
+}
+
+/**
+ * _strncpy - copies a string.
+ * @dest: string of destinatation.
+ * @src : string to copy.
+ * @n: bite limit.
+ * Return: dest.
+ **/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
 
 /**
  * *new_dog - creates a new dog..
@@ -17,24 +57,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 
 	if (new_dog == NULL)
-	{
 		return (NULL);
-	}
-	{
-	new_dog->name = strdup(name);
+
+	new_dog->name = malloc(_strlen(name) + 1);
 	if (new_dog->name == NULL)
-	{
-		free(new_dog);
 		return (NULL);
-	}
+	_strncpy(new_dog->name, name, _strlen(name) + 1);
 	new_dog->age = age;
-	new_dog->owner = strdup(owner);
+	new_dog->owner = malloc(_strlen(owner) + 1);
 	if (new_dog->owner == NULL)
-	{
-		free(new_dog->name);
-		free(new_dog);
 		return (NULL);
-	}
-	}
-	return (new_dog);
+	 _strncpy(new_dog->owner, owner, _strlen(owner) + 1);
+
+return (new_dog);
 }
--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -4,4 +4,5 @@
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
+char *_strdup(char *str);
 #endif
