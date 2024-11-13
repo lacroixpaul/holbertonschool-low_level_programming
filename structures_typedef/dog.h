db662f1a5dc8ff9f21101cf8c012399447fe359a
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-13 11:11:33 +0100

    Script 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of a dog.
+ * @name : of the dog.
+ * @age : of the dog.
+ * @owner : name of the owner.
+ **/
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} Dog;
+
+#endif
