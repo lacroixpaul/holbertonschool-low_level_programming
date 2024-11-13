a2110d742fbde27e381bef427df4020f0fd3a6c3
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-13 13:45:59 +0100

    Script 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -24,12 +24,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
 	new_dog->age = age;
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 	}
