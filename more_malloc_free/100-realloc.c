4760e38764bb9e50076073ceb1a8d0aaf0f5c70d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-14 11:07:50 +0100

    Script 100

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -50,5 +50,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		min_size = old_size < new_size ? old_size : new_size;
 		_memcpy(new_ptr, ptr, min_size);
+		free(ptr);
 	return (new_ptr);
 }
