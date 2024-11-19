7502f10b207f409737384e8db334ad3f0998b3df
lacroixpaul <paul.lacroix892@outlook.fr>
2024-11-19 16:09:43 +0100

    Script 3_en cours

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,7 +10,7 @@
 
 void print_char(va_list args)
 {
-printf("%c", va_arg(args, int));
+	printf("%c", va_arg(args, int));
 }
 
 /**
@@ -21,7 +21,7 @@ printf("%c", va_arg(args, int));
 
 void print_int(va_list args)
 {
-printf("%d", va_arg(args, int));
+	printf("%d", va_arg(args, int));
 }
 
 /**
@@ -32,7 +32,7 @@ printf("%d", va_arg(args, int));
 
 void print_float(va_list args)
 {
-printf("%f", va_arg(args, double));
+	printf("%f", va_arg(args, double));
 }
 
 /**
@@ -44,27 +44,15 @@ printf("%f", va_arg(args, double));
 void print_string(va_list args)
 {
 char *str = va_arg(args, char *);
+if (str == NULL)
+str = "(nil)";
 printf("%s", str);
 }
 
 /**
- * type_specifier_t: structure.
- * @specifier: The type.
- * @f: The function associated.
- */
-
-type_specifier_t specifiers[] =
-{
-{'c', print_char},
-{'i', print_int},
-{'f', print_float},
-{'s', print_string},
-{'\0', NULL}
-};
-
-/**
  * print_all- prints anything, followed by a new line.
  * @format : list of types of arguments passed to the function.
+ * type_specifier_t: structure.
  * Return: nothing.
  **/
 
@@ -73,6 +61,14 @@ void print_all(const char * const format, ...)
 unsigned int i = 0, j = 0;
 va_list args;
 va_start(args, format);
+
+type_specifier_t specifiers[] = {
+{'c', print_char},
+{'i', print_int},
+{'f', print_float},
+{'s', print_string},
+{'\0', NULL}
+};
 
 while (format[i] != '\0')
 {
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -7,12 +7,16 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
+/**
+* struct type_specifier - Structure de type spécificateur
+* @specifier: Un caractère représentant le spécificateur de type
+* @print_func: Pointeur vers une fonction
+**/
 typedef struct type_specifier
 {
 char specifier;
 void (*print_func)(va_list args);
-}
-type_specifier_t;
+} type_specifier_t;
 
 void print_float(va_list args);
 void print_char(va_list args);
