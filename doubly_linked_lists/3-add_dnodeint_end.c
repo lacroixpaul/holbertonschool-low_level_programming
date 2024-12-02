9ff6bd11cd20ec0e56696f276954108bf06cc9c6
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 15:08:25 +0100

    3-add_dnodeint betty compliant

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -23,10 +23,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 
 	if (*head == NULL)
-		{
-			new_node->prev = NULL;
-			*head = new_node;
-		}
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+	}
 	else
 	{
 		pCurrent = *head;
