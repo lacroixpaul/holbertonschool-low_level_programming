1bb320c63967c57a0efa6309c60e893646ccdf0d
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 15:03:54 +0100

    3-add_dnodeint checked and betty compliant

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head : last element added.
+ * @n : int to add.
+ * Return: the address of the new element, or NULL if it failed.
+ **/
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *pCurrent;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	new_node->next = NULL;
+	if (*head == NULL)
+		*head = new_node;
+	else
+	{
+		pCurrent = *head;
+			while (pCurrent->next != NULL)
+			{
+				pCurrent = pCurrent->next;
+			}
+		pCurrent->next = new_node;
+	}
+	return (new_node);
+}
