18cb7e5ed50f60c6b683fbed7f717f2df510df4f
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 14:42:05 +0100

    1-dlistint_len add checked and betty compliant

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *  dlistint_len - returns the number of elements in a list_t list.
+ * @h : pointer to the first node.
+ * Return: the number of elements.
+ **/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *element = h;
+
+	while (element != NULL)
+	{
+		count++;
+		element = element->next;
+	}
+	return (count);
+}
