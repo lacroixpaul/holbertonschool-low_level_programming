f6b2caf5000f8b4210af2394b514f017d3fb0cff
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 15:19:12 +0100

    4-free_dlistint checked and betyt compliant

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,7 +6,7 @@
 /**
 * free_dlistint - free a listint_t list.
 * @head : last element added.
-* Return: the address of the new element, or NULL if it failed.
+* Return: the address of the new element or NULL if it failed.
  **/
 
 void free_dlistint(dlistint_t *head)
