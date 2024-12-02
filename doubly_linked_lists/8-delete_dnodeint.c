e4d9b6c9f8b9b434a4dc4b074ff72efbba61c3a7
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 17:06:25 +0100

    8-delete_dnodeint_at_index checked and betty compliant

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - deletes the node at index of a linked list.
+* @head : double pointer to the last element.
+* @index :  is the index of the node, starting from 0.
+* Return: 1 if it succeeded, -1 if it failed.
+ **/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *pCurrent = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = pCurrent->next;
+		if (*head != NULL)
+		(*head)->prev = NULL;
+		free(pCurrent);
+		return (1);
+	}
+	while (pCurrent != NULL && count < index)
+	{
+		pCurrent = pCurrent->next;
+		count++;
+	}
+	if (pCurrent == NULL)
+		return (-1);
+	free(pCurrent);
+	return (1);
+}
