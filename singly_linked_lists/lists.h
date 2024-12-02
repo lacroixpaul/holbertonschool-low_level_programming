606675118c8d19730cfeb4b4af2ca79dedc134bf
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-02 09:50:47 +0100

    list.h inverted prototype and struc

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,9 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-int _putchar(char c);
-size_t print_list(const list_t *h);
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,5 +15,8 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
 
 #endif
