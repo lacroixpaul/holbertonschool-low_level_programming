1e2b17c3cb6744f52ce12de7ff07a3adba7cdb1e
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-03 15:00:38 +0100

    2-append_text_to_file checked and betty compliant

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename : name of the file.
+ * @text_content : a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure or if filename is NULL.
+ **/
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+	else
+		return (-1);
+
+	close(fd);
+	return (1);
+}
