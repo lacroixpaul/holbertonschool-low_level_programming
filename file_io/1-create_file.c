4bfc9bbf806fbe11fe2b08c3966c83afbae490dc
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-03 14:52:07 +0100

    1-create_file NULL filename case added

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,10 +16,14 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 
+	if (filename == NULL)
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		return (-1);
-	write(fd, text_content, strlen(text_content));
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
+
 	close(fd);
 	return (1);
 }
