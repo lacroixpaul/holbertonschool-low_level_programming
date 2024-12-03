4614e1ab3d8f37ca16e36c18d83846ab0a7cc992
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-03 14:49:41 +0100

    1-create_file add, checked and betty compliant

---
+++ file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * create_file - creates a file.
+ * @filename : name of the file.
+ * @text_content : a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure or if filename is NULL.
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd == -1)
+		return (-1);
+	write(fd, text_content, strlen(text_content));
+	close(fd);
+	return (1);
+}
