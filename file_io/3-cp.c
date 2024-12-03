3ea1b6cdb4ae1ef23235490e887d6d57468f91f8
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-03 19:14:51 +0100

    main.h : updated with error_code function, script 3 : code error updated

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,13 +6,26 @@
 #include <string.h>
 
 /**
+ * error_code - print message error.
+ * @message : message to print.
+ * @filename : of the file which doesn't work.
+ * @exit_code : code of the error.
+ **/
+
+void error_code(const char *message, const char *filename, int exit_code)
+{
+	dprintf(STDERR_FILENO, "Error: %s %s\n", message, filename);
+	exit(exit_code);
+}
+
+/**
  * main - copies the content of a file to another file.
  * @argc : number of arguments.
  * @argv : a first file to copy and a second to store.
- * Return: nothing.
+ * Return: 0 if success.
  **/
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int source, target;
 	ssize_t n_read, n_written;
@@ -23,26 +36,29 @@ void main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	source = open(argv[1], O_RDONLY);
 	if (source == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		error_code("Can't read from file", argv[1], 98);
 	target = open(argv[2], O_WRONLY | O_TRUNC);
 	if (target == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		error_code("Can't write to", argv[2], 99);
+		close(source);
 	}
 	while ((n_read = read(source, buffer, sizeof(buffer))) > 0)
 	{
 		n_written = write(target, buffer, n_read);
 		if (n_written != n_read)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
+			error_code("Can't write to", argv[2], 99);
+			close(target),	close(source);
 		}
 	}
+	if (n_read == -1)
+		error_code("Can't read from file", argv[1], 98);
+	if (close(source) == -1)
+		error_code("Can't close file %s", argv[1], 100);
+	if (close(target) == -1)
+		error_code("Can't close file %s", argv[2], 100);
+	return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -5,6 +5,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
-void main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+void error_code(const char *message, const char *filename, int exit_code);
 
 #endif
