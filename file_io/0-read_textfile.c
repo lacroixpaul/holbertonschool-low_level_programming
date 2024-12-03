3b74d44636e5ffc455a0ef6b7db6989b45caeeb1
lacroixpaul <paul.lacroix892@outlook.fr>
2024-12-03 12:22:32 +0100

    0-read_textfiles.c bytes_read and bytes_written added, script checked and betty compliant

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -15,6 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
 	if (filename == NULL)
 		return (0);
@@ -27,12 +28,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	read(fd, buffer, letters);
-	write(STDOUT_FILENO, buffer, letters);
-
-	close(fd);
-	if (close(fd) == -1)
+	bytes_read = read(fd, buffer, letters);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_read != bytes_written)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
-
-	return (letters);
+	}
+	close(fd);
+	return (bytes_read);
 }
